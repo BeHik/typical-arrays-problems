e577edd8186dce21a9826e2dd42b99f7ede2fe89
BeHik <behik3d@gmail.com>
2021-07-29 02:33:55 +0300

    feat: task completed

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minNum = 0;
+
+  if ((array!==undefined) && (array.length!==0)) {
+  array.forEach(element => {element<minNum ? minNum=element : '' });}
+  return minNum;
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxNum = 0;
+  if ((array!==undefined) && (array.length!==0)) {
+  array.forEach(element => {element>maxNum ? maxNum=element : ''});}
+  return maxNum;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let avgNum = 0;
+  if ((array===undefined) || (array.length===0)) return avgNum;
+
+  array.forEach(element => {avgNum += element});
+  
+  return avgNum/array.length;
 }
